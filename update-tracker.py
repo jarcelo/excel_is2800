99ecfde079e0dc300f94b8dd0816da7b818e4e7e
Joe Arcelo <josepharcelo@gmail.com>
2019-02-11 20:51:49 -0600

    Refactor with functions

--- update-tracker.py
+++ update-tracker.py
@@ -10,8 +10,10 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('is2800-1166f4019
 gsheet = gspread.authorize(credentials)
 # Open workbook
 workbook = gsheet.open('test')
+#workbook = gsheet.open('Excel_Case2_Scorecard_006')
 # Get worksheets count
 sheetCount = workbook.worksheets()
+
 # Format cells with zero grade values; change the background color
 bgfmt = CellFormat(
     backgroundColor=color(1, 0.9, 0.9)
@@ -20,23 +22,18 @@ scorefmt = CellFormat(
     textFormat = TextFormat(bold=True)
 )
 
-# Update grade tracker
-for index, item in enumerate(sheetCount):
-    print("Working on :"+ str(item))
-    # Get the worksheet
-    worksheet = workbook.get_worksheet(index)
-    # Get the column D values
-    items = worksheet.col_values(4)  
-    # Get the column number for last entry in column D
+def identifyIncorrectItem(items):
+    print("Indentifying incorrect items ... ")
     colDLength = len(items)
-    print("Identifying incorrect items ... ")
-    # Loop through the items. Change the background color if the values is zero
     for index, item in enumerate(items):
-        if item == '0'and index != 1 and index != colDLength - 1 :
+        if item == '0'and index > 1 and index < (colDLength - 1) :
             cellRange = "B" + str(index + 1) + ":" + "D" + str(index + 1)
             format_cell_range(worksheet, cellRange, bgfmt)
-            #print(index, item)  # Uncomment to print the items with zero score
+    print("Done.")
 
+
+def calculateScore(colDLength):
+    print("Calculating submission score ... ")
     percentTotal = '=sum(D2:D' + str(colDLength) + ')'
     scale = '=D' + str(colDLength + 1) + "/100*25"
     percentCell = 'D' + str(colDLength + 1)
@@ -44,9 +41,36 @@ for index, item in enumerate(sheetCount):
     scoreCellRange = scoreCell + ":" + scoreCell
     format_cell_range(worksheet, scoreCellRange, scorefmt)
 
-    #Get sum and score
-    print("Calculating submission score ... ")
     worksheet.update_acell(percentCell, percentTotal)
     worksheet.update_acell(scoreCell, scale)
     print("Done.")
-    print("------------------------------------------------")
+
+
+# Update grade tracker
+start = -1
+for index, item in enumerate(sheetCount):
+    if index > start:
+        print("Working on : "+ str(index) + "  " + str(item))
+        # Get the worksheet
+        worksheet = workbook.get_worksheet(index)
+        #worksheet = workbook.get_worksheet(i)
+        # Get the column D values
+        items = worksheet.col_values(4)
+        #print("Indentifying incorrect items ... ")
+        #identifyIncorrectItem(items)
+        #for index, item in enumerate(items):
+        #    if item == '0'and index > 1 and index < (colDLength - 1) :
+        #        cellRange = "B" + str(index + 1) + ":" + "D" + str(index + 1)
+        #        format_cell_range(worksheet, cellRange, bgfmt)
+        #        for
+        #        print(index, item)  # Un/comment to print the items with zero score
+
+        #print(worksheet.col_values(4))
+        # Get the column number for last entry in column D
+        colDLength = len(items)
+        calculateScore(colDLength)
+     
+        #print(worksheet.col_values(4))
+        #i+=1
+        #print("Current sheet id: " + str(1))
+        print("----------------------------------------------------")
